d29223265e35db857632f5272298acb0b97b184a
bahaeddine10 <bahagassar@gmail.com>
2024-05-30 02:58:49 +0200

    changing contact me !

--- js/script.js
+++ js/script.js
@@ -54,23 +54,33 @@ contact.addEventListener("click",()=>{
     list[list.length-1].classList.add("active");
     list[list.length-1].click();
     
-    const btn = document.getElementById('button');
+    
 
     document.getElementById('form').addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+  
 
    const serviceID = 'default_service';
    const templateID = 'template_yckzbbg';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
+     
+        Swal.fire({
+            icon: "success",
+            title: "Send Success",
+            
+          });
+          
     }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+        Swal.fire({
+            icon: "error",
+            title: "Send Fail",
+            text: JSON.stringify(err),
+          });
+          
+      
     });
 });
 
